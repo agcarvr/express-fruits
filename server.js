da3f06fb4d217714614affc12a6450ad63cfbadf
aaroncarver <agcarvr@gmail.com>
2021-04-29 07:16:37 -0600

    removed excersizes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 // import express from 'express'
 // require all my modules
 require('dotenv').config()
-console.log(process.env.MONGO_URI);
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Fruit = require('./models/Fruit');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+
+//define my database and middleware
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+})
+
+mongoose.connection.once('connected', () => console.log('Connected to mongo life is good'))
 
 
 const fruits = [
@@ -31,8 +42,15 @@ const fruits = [
 // restful routs
 // INDUCES
 //Index
-app.get('/fruits', (req, res) => {
-    res.status(200).json(fruits)
+app.get('/fruits', async (req, res) => {
+    try{
+        const foundFruits = await Fruit.find({});
+        res.status(200).json(foundFruits)        
+    }catch(error){
+        res.status(400).json({
+            msg: error.message
+        })
+    }
 })
 
 //New
@@ -61,30 +79,11 @@ app.get('/fruits/:id', (req, res) => {
     }
 })
 
-app.get('/fruits/:id/display', (req, res) => {
-    const id = parseInt(req.params.id)
-    if (id !== id){
-        res.status(404).send(`ur bad try again`)
-    }else if(id >= fruits.length || id < 0){
-        res.status(404).send(`<p>yeah no bud</p>`)
-    }else {
-        res.status(200).send(`<p>Your fruit is ${fruits[id].name}, it is ${fruits[id].color} and ${fruits[id].isReadyToEat? "is ready to eat": "is not ready to eat"}</p>`)
-    }
-})
 
 
 app.get('/', (request, response) => {
     response.send(`<h1>Something else</h1>`)
 })
-
-app.get('/hello/:name', (req, res) => {
-    res.send(`<h1>Hello ${req.params.name} its nice to meet you<h1>`)
-})
-
-app.get('/:thing', (req, res) => {
-    res.send(`<h1>${req.params.thing}<h1>`)
-})
-
 
 
 
