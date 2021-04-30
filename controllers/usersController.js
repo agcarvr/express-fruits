2e509340621e44eaa38dbf966f4ab4f8fce58fdd
aaroncarver <agcarvr@gmail.com>
2021-04-30 10:28:44 -0600

    made vegetable model

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,37 +1,80 @@
 const express = require('express');
-const User = require('../models/User')
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/User');
+const Fruit = require('../models/Fruit');
 
 
 router.get('/', (req, res) => {
-    const userQuery = User.find({}).select('-password').populate('fruits');
+   const userQuery = User.find({}).select('-password').populate('fruits') 
+   userQuery.exec((err, foundUsers) => {
+       if (err){
+           console.log(err);
+           res.status(401).json({ msg: err.message });
+       } else {
+          res.status(200).json(foundUsers) 
+       }
+   })
+})
 
-    userQuery.exec((err, foundUsers) => {
-        if(err){
-            console.log(err);
-            res.status(401).json({msg: err.message});
-        }else{
-            res.status(200).json(foundUsers)
+
+// add fruits to users
+// existing
+router.post('/addFruitToUser/:username', (req, res) =>{
+    const fruit = req.body
+    const addFruitQuery = User.findOneAndUpdate({ username: req.params.username }, { $addToSet: { fruits: fruit._id }}, {new: true})
+    addFruitQuery.exec((err, updatedUser) => {
+        if (err){
+            res.status(400).json({
+                msg: err.message
+            })
+        } else {
+            res.status(200).json({
+                msg: 'Update User with ' + fruit.name
+            })
         }
     })
 })
 
 
-router.post('/addFruitToUser/:username', (req, res) => {
-    const fruit = req.body;
-    User.findOneAndUpdate({username: req.body.username}, { $addToSet: {fruits: fruit._id}}, {new: true})
-    addFruitQuery.exec((err, updatedUser) => {
+router.post('/addFruit/:fruit/:username', (req, res) =>{
+    const fruitQuery = Fruit.findOne({ name: req.params.fruit })
+    fruitQuery.exec(( err, fruit ) => {
         if(err){
             res.status(400).json({
                 msg: err.message
             })
-        }else{
-            res.status(200).json({
-                msg: 'Updated user with ' + fruit.name
+        } else {
+            const addFruitQuery = User.findOneAndUpdate({ username: req.params.username }, { $addToSet: { fruits: fruit._id }}, {new: true})
+            addFruitQuery.exec((err, updatedUser) => {
+                if(err){
+                    res.status(400).json({
+                        msg: err.message
+                    }) 
+                } else {
+                    console.log(updatedUser);
+                    res.status(200).json({
+                        msg: `Updated ${updatedUser.username} with the fruit ${fruit.name} `
+                    })
+                }
             })
         }
     })
 })
 
+router.get('/:username', (req, res) => {
+    const userQuery = User.findOne({username: req.params.username}).select('-password').populate('fruits')
 
+    userQuery.exec((err, foundUser) => {
+        if(err){
+                res.status(400).json({
+                msg: err.message
+            })
+        } else{
+            res.status(200).json(foundUser)
+        }
+    })
+})
+
+
+// shows all fruits for a specific user
 module.exports = router
